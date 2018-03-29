648121cc5d2371e0338edd90492c83d1e33725ed
Jason Cochrane <jason.cochrane@boeing.com>
2018-03-29 16:49:12 +1100

    - Add backwards compatibility for gz7

--- src/planar_move.cpp
+++ src/planar_move.cpp
@@ -133,18 +133,27 @@ void PlanarMove::UpdateChild()
     if (control_mode_ == "position")  // Position control mode
     {
         // Get the simulation time and period
+        math::Pose current_pose = parent_->GetWorldPose();
+
+#if GAZEBO_MAJOR_VERSION >= 8
         double gz_time_now = parent_->GetWorld()->SimTime().Double();
+        ignition::math::Pose3d current_pose = parent_->WorldPose();
+        double current_yaw = current_pose.Rot().Yaw();
+        ignition::math::Pose3d new_pose;
+#else
+        double gz_time_now = parent_->GetWorld()->GetSimTime().Double();
+        double current_yaw = current_pose.rot.GetYaw();
+        math::Pose new_pose;
+#endif
+
         double dt =  gz_time_now - gz_time_last;
         gz_time_last =  gz_time_now;
-
-        ignition::math::Pose3d current_pose = parent_->WorldPose();
-        ignition::math::Pose3d new_pose;
-        double current_yaw = current_pose.Rot().Yaw();
 
         // ROS_INFO_STREAM("Command: x:" << x_ << " y:" << y_ << " rotZ:" << rot_);
         // ROS_INFO_STREAM("Current Pose: x:" <<  current_pose.pos.x << " y:" <<  current_pose.pos.y << " Yaw:" <<  current_pose.rot.GetYaw());
 
         // Forward velocity component (x)
+#if GAZEBO_MAJOR_VERSION >= 8
         new_pose.Pos().X() = current_pose.Pos().X()+ dt*x_*cos(current_yaw);  // We need to do this so dx and dy are in world frame
         new_pose.Pos().Y() = current_pose.Pos().Y()+ dt*x_*sin(current_yaw);  // since cmd_vel is in robot frame
 
@@ -152,7 +161,15 @@ void PlanarMove::UpdateChild()
         new_pose.Pos().X() = new_pose.Pos().X() - dt*y_*cos(M_PI / 2 - current_yaw);  // We need to do this so dx and dy are in world frame
         new_pose.Pos().Y() = new_pose.Pos().Y() + dt*y_*sin(M_PI / 2 - current_yaw);  // since cmd_vel is in robot frame
         new_pose.Rot().Euler(0.0, 0.0, current_yaw + dt * rot_);
+#else
+        new_pose.pos.x = current_pose.pos.x+ dt*x_*cos(current_yaw);  // We need to do this so dx and dy are in world frame
+        new_pose.pos.y = current_pose.pos.y+ dt*x_*sin(current_yaw);  // since cmd_vel is in robot frame
 
+        // Strafing velocity component (y)
+        new_pose.pos.x = new_pose.pos.x - dt*y_*cos(M_PI / 2 - current_yaw);  // We need to do this so dx and dy are in world frame
+        new_pose.pos.y = new_pose.pos.y + dt*y_*sin(M_PI / 2 - current_yaw);  // since cmd_vel is in robot frame
+        new_pose.rot.SetFromEuler(0.0, 0.0, current_yaw + dt * rot_);
+#endif
         if (base_link_ == NULL)
         {
             ROS_FATAL_THROTTLE(1, "Model has no link named 'base link'");
@@ -161,6 +178,7 @@ void PlanarMove::UpdateChild()
         {
             // ROS_INFO_STREAM("Setting link, dt is:" << dt << " new x:" << new_pose.pos.x << " new y:" << new_pose.pos.y << " new yaw:" << new_pose.rot.GetYaw());
             parent_->SetLinkWorldPose(new_pose, base_link_);
+
             // Hack disables physics, required after call to any physics related function call
             for (physics::LinkPtr link : links_list_)
             {
@@ -172,10 +190,17 @@ void PlanarMove::UpdateChild()
     {
         if (new_cmd_)
         {
+#if GAZEBO_MAJOR_VERSION >= 8
             ignition::math::Pose3d pose = parent_->WorldPose();
             float yaw = pose.Rot().Yaw();
             parent_->SetLinearVel(ignition::math::Vector3d(x_ * cosf(yaw) - y_ * sinf(yaw), y_ * cosf(yaw) + x_ * sinf(yaw), 0));
             parent_->SetAngularVel(ignition::math::Vector3d(0, 0, rot_));
+#else
+            math::Pose pose = parent_->GetWorldPose();
+            float yaw = pose.rot.GetYaw();
+            parent_->SetLinearVel(math::Vector3(x_ * cosf(yaw) - y_ * sinf(yaw), y_ * cosf(yaw) + x_ * sinf(yaw), 0));
+            parent_->SetAngularVel(math::Vector3(0, 0, rot_));
+#endif
             new_cmd_ = false;
         }
     }
@@ -220,6 +245,7 @@ void PlanarMove::publishOdometry()
     ros::Time current_time = ros::Time::now();
 
     // getting data for base_footprint to odom transform
+#if GAZEBO_MAJOR_VERSION >= 8
     ignition::math::Pose3d pose = parent_->WorldPose();
 
     tf::Quaternion qt(pose.Rot().X(), pose.Rot().Y(), pose.Rot().Z(), pose.Rot().W());
@@ -253,6 +279,42 @@ void PlanarMove::publishOdometry()
     odom.twist.twist.angular.x = rot_velocity.X();
     odom.twist.twist.angular.y = rot_velocity.Y();
     odom.twist.twist.angular.z = rot_velocity.Z();
+
+#else
+    math::Pose pose = parent_->GetWorldPose();
+
+    tf::Quaternion qt(pose.rot.x, pose.rot.y, pose.rot.z, pose.rot.w);
+    tf::Vector3 vt(pose.pos.x, pose.pos.y, pose.pos.z);
+
+    tf::Transform base_footprint_to_odom(qt, vt);
+    transform_broadcaster_.sendTransform(tf::StampedTransform(base_footprint_to_odom, current_time, odometry_frame_, robot_base_frame_));
+
+    // publish odom topic
+    nav_msgs::Odometry odom;
+    odom.pose.pose.position.x = pose.pos.x;
+    odom.pose.pose.position.y = pose.pos.y;
+
+    odom.pose.pose.orientation.x = pose.rot.x;
+    odom.pose.pose.orientation.y = pose.rot.y;
+    odom.pose.pose.orientation.z = pose.rot.z;
+    odom.pose.pose.orientation.w = pose.rot.w;
+    odom.pose.covariance[0] = 0.00001;
+    odom.pose.covariance[7] = 0.00001;
+    odom.pose.covariance[14] = 1000000000000.0;
+    odom.pose.covariance[21] = 1000000000000.0;
+    odom.pose.covariance[28] = 1000000000000.0;
+    odom.pose.covariance[35] = 0.001;
+
+    math::Vector3 linear_velocity = parent_->GetRelativeLinearVel();
+    odom.twist.twist.linear.x = linear_velocity.x;
+    odom.twist.twist.linear.y = linear_velocity.y;
+    odom.twist.twist.linear.z = linear_velocity.z;
+
+    math::Vector3 rot_velocity = parent_->GetRelativeAngularVel();
+    odom.twist.twist.angular.x = rot_velocity.x;
+    odom.twist.twist.angular.y = rot_velocity.y;
+    odom.twist.twist.angular.z = rot_velocity.z;
+#endif
 
     odom.header.stamp = current_time;
     odom.header.frame_id = odometry_frame_;
