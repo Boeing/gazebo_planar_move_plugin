cea494cfa137e944a6e965860d111ebb88b33f0b
Ivan Villaverde De La Nava <ivan.villaverde@tecnalia.com>
2022-12-14 10:45:45 +0100

    Add test

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -1,26 +1,173 @@
-#!/usr/bin/env python3
-
+import os
 import unittest
 
-import rospy
-import rostest
-from gazebo_msgs.srv import GetLinkState
-from gazebo_msgs.srv import GetLinkStateRequest, GetLinkStateResponse
+import launch
+import launch.actions
+
+import launch_testing
+import launch_testing.actions
+from ament_index_python import get_package_share_directory
+
+import subprocess
+import rclpy
+import rclpy.clock
+import rclpy.time
+from rclpy.qos import DurabilityPolicy, HistoryPolicy, QoSProfile
+import pytest
+from rclpy.parameter import Parameter
+
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+from gazebo_msgs.srv import GetEntityState
 from geometry_msgs.msg import Twist
 
+from time import sleep
 
-class TestPlugin(unittest.TestCase):
+@pytest.mark.launch_test
+def generate_test_description():
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    world_file_name = os.path.join(get_package_share_directory('gazebo_planar_move_plugin'),
+                         'test', 'test.world')
+    urdf_file_name = os.path.join(get_package_share_directory('gazebo_planar_move_plugin'),
+                         'test', 'test.urdf')
+
+    print('robot  urdf_file_name : {}'.format(urdf_file_name))
+    print('world world_file_name : {}'.format(world_file_name))
+
+    return launch.LaunchDescription(
+        [
+        # Launch GAZEBO
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+            ),
+            launch_arguments={'world': world_file_name, 'gui': '0'}.items(),
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+            ),
+            launch_arguments={'gui': '0'}.items(),
+        ),
+
+        # Launch robot_state_publisher
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': True}],
+            arguments=[urdf_file_name]
+        ),
+
+        # Spawn robot in Gazebo
+        Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-entity', 'test_robot', '-file', urdf_file_name],
+                        output='screen'),
+
+        launch_testing.actions.ReadyToTest(),
+        ]
+    )
+
+
+# There is a bug where the gzserver is not killed after the tests run.
+# https://github.com/ros2/launch/issues/545
+# The issue should be fixed by https://github.com/ros-simulation/gazebo_ros_pkgs/pull/1376
+# This is a workaround to kill the gzserver after the tests run.
+# Remove this once gazebo updates the apt package to the latest version.
+@launch_testing.post_shutdown_test()
+class TestShutdown(unittest.TestCase):
+    def test_kill_sim(self):
+        subprocess.run(["pkill", "gzserver"])
+        subprocess.run(["pkill", "gzclient"])
+
+
+# These tests will run concurrently with the dut process.  After all these tests are done,
+# the launch system will shut down the processes that it started up
+class TestPlanarMovePlugin(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
-        cls.__get_link_state_srv = rospy.ServiceProxy(
-            name='/gazebo/get_link_state',
-            service_class=GetLinkState
-        )
-        cls.__get_link_state_srv.wait_for_service(timeout=30)
+        rclpy.init()
 
-        cls.__cmd_vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=100, latch=True)
+    @classmethod
+    def tearDownClass(cls):
+        rclpy.shutdown()
 
-        cls.__test_link_name = 'base_link'
+    def setUp(self):
+        self.node = rclpy.create_node('test_node', parameter_overrides=[
+                Parameter('use_sim_time', Parameter.Type.BOOL, True)])
+        self.clock = rclpy.clock.Clock(clock_type=rclpy.clock.ClockType.ROS_TIME)
+        self.log = self.node.get_logger()
+
+        # Client for checking entity status in gazebo 
+        self.entity_state_client = self.node.create_client(GetEntityState, '/gazebo/get_entity_state')
+        while not self.entity_state_client.wait_for_service(timeout_sec=1.0):
+            self.log.info('Entity state service not available, waiting again...')
+
+        # Twist publisher with latching QoS
+        qos_profile = QoSProfile(depth=100, durability=DurabilityPolicy.TRANSIENT_LOCAL, history=HistoryPolicy.KEEP_LAST)
+        self.twist_publisher = self.node.create_publisher(Twist, 'cmd_vel', qos_profile)
+
+        self.robot_name = 'test_robot'
+        self.world_frame = 'world'
+
+        sleep(5) # Give time to Gazebo client/server to bring up
+
+
+    def tearDown(self):
+        self.node.destroy_node()
+
+    def set_and_test_velocity(self, v_x, v_y, v_z, v_rz):
+        # Set test velocity
+        test_twist = self.make_twist(self, v_x, v_y, v_z, v_rz)
+        self.twist_publisher.publish(test_twist)
+
+        sleep(10) # Small sleep to wait for the set to take effect
+
+        # Get entity velocity
+        self.entity_state_req = GetEntityState.Request()
+        self.entity_state_req.name = self.robot_name
+        self.entity_state_req.reference_frame = self.world_frame
+        self.entity_state_future = self.entity_state_client.call_async(self.entity_state_req)
+        rclpy.spin_until_future_complete(self.node, self.entity_state_future)
+        self.entity_state_res = self.entity_state_future.result()
+
+        self.node.get_logger().info('Entity state retrieved as value: ''{}'' \nexpected ''{}'''
+                      .format(self.entity_state_res.state.twist, test_twist))
+        assert isinstance(self.entity_state_res, GetEntityState.Response)
+        self.assertTrue(self.entity_state_res.success)
+
+        self.assertAlmostEqual(
+            self.entity_state_res.state.twist.linear.x, test_twist.linear.x, delta=0.01)
+        self.assertAlmostEqual(
+            self.entity_state_res.state.twist.linear.y, test_twist.linear.y, delta=0.01)
+        self.assertAlmostEqual(
+            self.entity_state_res.state.twist.linear.z, test_twist.linear.z, delta=0.01)
+        self.assertAlmostEqual(
+            self.entity_state_res.state.twist.angular.z, test_twist.angular.z, delta=0.01)
+
+        test_twist = self.make_twist(self, 0.0, 0.0, 0.0, 0.0)
+        self.twist_publisher.publish(test_twist)
+
+
+    @staticmethod
+    def make_twist(self, x, y, z, rz):
+        test_twist = Twist()
+        test_twist.linear.x = x
+        test_twist.linear.y = y
+        test_twist.linear.z = z
+        test_twist.angular.x = 0.0
+        test_twist.angular.y = 0.0
+        test_twist.angular.z = rz
+        return test_twist
+
 
     def test_set(self):
         # Test linear moves
@@ -60,49 +207,3 @@ class TestPlugin(unittest.TestCase):
                                    v_y=1.0,
                                    v_z=0.0,
                                    v_rz=0.5)
-
-    def set_and_test_velocity(self, v_x, v_y, v_z, v_rz):
-        # Set test velocity
-        test_twist = self.make_twist(self, v_x, v_y, v_z, v_rz)
-        self.__cmd_vel_pub.publish(test_twist)
-
-        rospy.sleep(0.5)  # Small sleep to wait for the set to take effect
-
-        # Get link velocity
-        get_link_response = self.__get_link_state_srv.call(
-            GetLinkStateRequest(link_name=self.__test_link_name,
-                                reference_frame='world'))
-
-        rospy.loginfo('Link state retrieved as value: ''{}'' \nexpected ''{}'''
-                      .format(get_link_response.link_state.twist, test_twist))
-        assert isinstance(get_link_response, GetLinkStateResponse)
-        self.assertTrue(get_link_response.success)
-
-        self.assertAlmostEqual(
-            get_link_response.link_state.twist.linear.x, test_twist.linear.x, delta=0.01)
-        self.assertAlmostEqual(
-            get_link_response.link_state.twist.linear.y, test_twist.linear.y, delta=0.01)
-        self.assertAlmostEqual(
-            get_link_response.link_state.twist.linear.z, test_twist.linear.z, delta=0.01)
-        self.assertAlmostEqual(
-            get_link_response.link_state.twist.angular.z, test_twist.angular.z, delta=0.01)
-
-        test_twist = self.make_twist(self, 0.0, 0.0, 0.0, 0.0)
-        self.__cmd_vel_pub.publish(test_twist)
-
-    @staticmethod
-    def make_twist(self, x, y, z, rz):
-        test_twist = Twist()
-        test_twist.linear.x = x
-        test_twist.linear.y = y
-        test_twist.linear.z = z
-        test_twist.angular.x = 0.0
-        test_twist.angular.y = 0.0
-        test_twist.angular.z = rz
-        return test_twist
-
-
-if __name__ == '__main__':
-    rospy.init_node('test_plugin')
-
-    rostest.rosrun('gazebo_planar_move_plugin', 'test_plugin', TestPlugin)
